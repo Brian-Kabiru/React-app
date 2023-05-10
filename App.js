46223db74918d3d4d98025a39834785f48703f85
Brian Kabiru <117152604+Brian-Kabiru@users.noreply.github.com>
2023-05-10 14:33:23 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <></>
+      <h1>My Workspace</h1>
+      <ul className="list">
+        <li>Learn and master react</li>
+        <li>Practise react</li>
+        <li>build PROJECTS</li>
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
