a0f6ae464024dec68f5b45e20dcdc98274cbb2ef
Brian Kabiru <117152604+Brian-Kabiru@users.noreply.github.com>
2023-05-11 13:50:40 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-
-function App() {
-  return (
-    <div className="App">
-      <></>
-      <h1>My Workspace</h1>
-      <ul className="list">
-        <li>Learn and master react</li>
-        <li>Practise react</li>
-        <li>build PROJECTS</li>
-
-      </ul>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';  
+class App extends Component {  
+ constructor() {  
+      super();        
+      this.state = { displayBio: false };  
+      console.log('Component this', this);  
+      this.toggleDisplayBio = this.toggleDisplayBio.bind(this);  
+      }  
+      toggleDisplayBio(){  
+          this.setState({displayBio: !this.state.displayBio});  
+          }  
+      render() {  
+          return (  
+              <div>  
+                  <h1>Welcome Brian!!</h1>  
+                  {  
+                      this.state.displayBio ? (   
+                          <div>  
+                              <p><h4>We can change the component state by using the setState() method and passing a new state object as the argument. Now, create a new method toggleDisplayBio() in the above example and bind this keyword to the toggleDisplayBio() method otherwise we can't access this inside  method. </h4></p>  
+                              <button onClick={this.toggleDisplayBio}> Show Less </button>  
+                        </div>  
+                          ) : (  
+                              <div>  
+                                  <button onClick={this.toggleDisplayBio}> Read More </button>  
+                              </div>  
+                          )  
+                  }  
+             </div>  
+        )  
+    }  
+}  
+export default App;  
